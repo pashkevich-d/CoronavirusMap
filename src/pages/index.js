ff24a0ad4366b27fb6f6a8f2f4fa95d8fb4c617e
Dmitry Pashkevich <dima-vanjuve@mail.ru>
2020-04-05 18:15:09 +0300

    add data to map

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Helmet from 'react-helmet';
-import L from 'leaflet';
+import L, { latLng } from 'leaflet';
 import axios from 'axios';
 
 
@@ -11,8 +11,8 @@ import Map from 'components/Map';
 import gatsby_astronaut from 'assets/images/gatsby-astronaut.jpg';
 
 const LOCATION = {
-  lat: 38.9072,
-  lng: -77.0369
+  lat: 0,
+  lng: 0
 };
 const CENTER = [LOCATION.lat, LOCATION.lng];
 const DEFAULT_ZOOM = 2;
@@ -31,6 +31,82 @@ const IndexPage = () => {
     }
 
     const {data = []} = response;
+    const hasData = Array.isArray(data) && data.length > 0;
+
+    if(!hasData) return;
+
+    const geoJson = {
+      type: "FeatureCollection",
+      features: data.map((country = {}) => {
+        const {countryInfo = {}} = country;
+        const {lat, long: lng} = countryInfo;
+        const flag = country.countryInfo.flag;
+        return {
+          type: 'Feature',
+          properties: {
+            ...country,
+            flag
+          },
+          geometry: {
+            type: 'Point',
+            coordinates: [lng, lat]
+          }
+        }
+      })
+    }
+
+    const geoJsonLayers = new L.GeoJSON(geoJson, {
+      pointToLayer: (feature = {}, latlng) => {
+        const {properties = {}} = feature;
+        let updatedFormatted;
+        let casesString;
+
+        const {
+          country,
+          updated,
+          cases,
+          deaths,
+          recovered,
+          flag
+        } = properties
+
+        casesString = `${cases}`;
+
+        if(cases> 1000){
+          casesString = `${casesString.slice(0, -3)}k+`
+        }
+
+        if (updated) {
+          updatedFormatted = new Date(updated).toLocaleDateString();
+        }
+
+        const html = `
+          <span class='icon-marker'>
+            <span class='icon-marker-tooltip'>
+              <h2>${country}</h2>
+              <img src="${flag}" >
+              <ul>
+                <li><strong>Confirmed:</strong> ${cases}</li>
+                <li><strong>Deaths:</strong> ${deaths}</li>
+                <li><strong>Recovered:</strong> ${recovered}</li>
+                <li><strong>Last Update:</strong> ${updatedFormatted}</li>
+              </ul>
+            </span>
+            ${ casesString }
+          </span>
+        `;
+
+        return L.marker( latlng, {
+          icon: L.divIcon({
+            className: 'icon',
+            html
+          }),
+          riseOnHover: true
+        });
+      }
+    });
+
+    geoJsonLayers.addTo(map);
   }
 
   const mapSettings = {
@@ -48,14 +124,14 @@ const IndexPage = () => {
 
       <Map {...mapSettings}/>
 
-      <Container type="content" className="text-center home-start">
+      {/* <Container type="content" className="text-center home-start">
         <h2>Still Getting Started?</h2>
         <p>Run the following in your terminal!</p>
         <pre>
           <code>gatsby new [directory] https://github.com/colbyfayock/gatsby-starter-leaflet</code>
         </pre>
         <p className="note">Note: Gatsby CLI required globally for the above command</p>
-      </Container>
+      </Container> */}
     </Layout>
   );
 };
